0b60c3e361c293860688aa557fa35a7b11ce44c3
Ryan Sproule <rsproule23@gmail.com>
2022-02-28 16:23:29 -0800

    update link for hosted version

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 The original plan for this project was to also give the ability to register via a smart contract to snipe an ENS address whenever it expires. This was before the ENS governence bumped the "premium" fee up from $2,000 to 100,000. After this change, this strategy is less interesting. Still it is a useful enough interface for doing some manual research for upcoming expirees. 
 
+## How to run:
+Clone the repo, then run
+```
+npm install
+npm run start
+```
+
 ## Hosted Version
-Currently being hosted by me here [`https://enssearcher.com`](https://enssearcher.com)
+Currently being hosted by me here [`https://enssearcher.com`](http://enssearcher.com)
 
 ![screenshot](assets/screenshot-scores.png)
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import { useQuery } from '@apollo/react-hooks'
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Body } from './components'
 import useWeb3Modal from './hooks/useWeb3Modal'
-
-import { GET_TRANSFERS } from './graphql/subgraph'
 import { ENSSingleNamePage } from './pages/ENSSingleNamePage'
 import { SearchENSPage } from './pages/SearchENSPage'
 import { ENSUpcomingExpiriesPage } from './pages/lists/ENSUpcomingExpiryListPage'
 import { NavBar } from './components/NavBar'
 
 function App() {
-  const { loading, error, data } = useQuery(GET_TRANSFERS)
   const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal()
-
-  React.useEffect(() => {
-    if (!loading && !error && data && data.transfers) {
-      console.log({ transfers: data.transfers })
-    }
-  }, [loading, error, data])
 
   return (
     <div>
