030688e014fc1525a02dfb53b2aacbaa67e952a9
Ryan Sproule <rsproule23@gmail.com>
2022-03-18 16:05:02 -0700

    remove all provider stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Body } from './components'
-import useWeb3Modal from './hooks/useWeb3Modal'
 import { ENSSingleNamePage } from './pages/ENSSingleNamePage'
 import { SearchENSPage } from './pages/SearchENSPage'
 import { ENSUpcomingExpiriesPage } from './pages/lists/ENSUpcomingExpiryListPage'
 import { NavBar } from './components/NavBar'
 
 function App() {
-  const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal()
+  // const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal()
 
   return (
     <div>
       <NavBar
-        provider={provider}
-        loadWeb3Modal={loadWeb3Modal}
-        logoutOfWeb3Modal={logoutOfWeb3Modal}
       />
 
       <Body>
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,36 +1,9 @@
 //// *** Uncomment all the code here to add the ability to login with wallet 
 //// *** in case you want to interact with smart contracts
 
-// import { useEffect, useState } from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
-// import { WalletButton } from '../WalletButton'
-
-interface NavBarProps {
-  provider: any
-  loadWeb3Modal: any
-  logoutOfWeb3Modal: any
-}
-
-export const NavBar = (props: NavBarProps) => {
-  // const { provider, loadWeb3Modal, logoutOfWeb3Modal } = props
-  // const [account, setAccount] = useState<string>('')
-  // useEffect(() => {
-  //   async function fetchAccount() {
-  //     try {
-  //       if (!provider) {
-  //         return
-  //       }
-  //       // Load the user's accounts.
-  //       const accounts = await provider.listAccounts()
-  //       setAccount(accounts[0])
-  //     } catch (err) {
-  //       setAccount('')
-  //       console.error(err)
-  //     }
-  //   }
-  //   fetchAccount()
-  // }, [account, provider, setAccount])
+export const NavBar = () => {
 
   return (
     <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
@@ -39,27 +12,14 @@ export const NavBar = (props: NavBarProps) => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            {/* <Nav.Link href="/">Home</Nav.Link> */}
             <LinkContainer to="/">
               <Nav.Link>Expiring Soon!</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/search">
               <Nav.Link>Search</Nav.Link>
             </LinkContainer>
-            {/* {account ? (
-              <LinkContainer to={`/accounts/${account}/snipes`}>
-                <Nav.Link>My Snipes</Nav.Link>
-              </LinkContainer>
-            ) : (
-              ''
-            )} */}
           </Nav>
         </Navbar.Collapse>
-        {/* <WalletButton
-          provider={provider}
-          loadWeb3Modal={loadWeb3Modal}
-          logoutOfWeb3Modal={logoutOfWeb3Modal}
-        /> */}
       </Container>
     </Navbar>
   )
